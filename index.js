14e5fde551aac25808a7fd05945074dacc7dcb85
mazyar-ahsani <76615477+mazyar-ahsani@users.noreply.github.com>
2025-05-03 18:29:05 +0330

    a tool to posts on x

---
+++ index.js
@@ -0,0 +1,19 @@
+const { TwitterApi } = require('twitter-api-v2');
+require('dotenv').config();
+
+// کلیدها و توکن‌ها رو از محیط بخون
+const client = new TwitterApi({
+  appKey: process.env.APP_KEY,
+  appSecret: process.env.APP_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_SECRET,
+});
+
+(async () => {
+  try {
+    await client.v2.tweet('سلام دنیا! این یه توییت اتوماتیکه 😎');
+    console.log('توییت با موفقیت ارسال شد.');
+  } catch (error) {
+    console.error('خطا در ارسال توییت:', error);
+  }
+})();
